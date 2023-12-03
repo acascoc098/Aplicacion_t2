4467b84e927f493901a16f0baf7485a836404a46
acascoc098 <acascoc098@g.educaand.es>
2023-12-03 23:25:45 +0100

    Cambio en el resultado, cambiamos los euros por cartas de póker y añadimos una imagenView para mostrar el resultado con las cartas.

--- app/src/main/java/com/example/aplicacion_t2/Dados.kt
+++ app/src/main/java/com/example/aplicacion_t2/Dados.kt
@@ -19,7 +19,20 @@ class Dados : AppCompatActivity() {
     private lateinit var imagen1: ImageView
     private lateinit var imagen2: ImageView
     private lateinit var imagen3: ImageView
-    private lateinit var resultado: TextView
+    private lateinit var resultado: ImageView
+    /*private lateinit var imagen1c: ImageView
+    private lateinit var imagen2c: ImageView
+    private lateinit var imagen3c: ImageView
+    private lateinit var imagen4: ImageView
+    private lateinit var imagen5: ImageView
+    private lateinit var imagen6: ImageView
+    private lateinit var imagen7: ImageView
+    private lateinit var imagen8: ImageView
+    private lateinit var imagen9: ImageView
+    private lateinit var imagen10: ImageView
+    private lateinit var imagenj: ImageView
+    private lateinit var imagenq: ImageView
+    private lateinit var imagenk: ImageView*/
     private var sum : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +41,8 @@ class Dados : AppCompatActivity() {
         imagen1 = findViewById(R.id.imagview_dado1)
         imagen2 = findViewById(R.id.imagview_dado2)
         imagen3 = findViewById(R.id.imagview_dado3)
-        resultado = findViewById(R.id.resultado)
+        resultado = findViewById(R.id.imagview_resultado)
+
         setContentView(R.layout.dados)
         initEvent()
 
@@ -113,7 +127,25 @@ class Dados : AppCompatActivity() {
     Muestra los resultados, que es la suma de la última tirada.
      */
     private fun viewResult() {
-        resultado.text = sum.toString()
-        println(sum)
+        when(sum){
+            1 -> resultado.setImageResource(R.drawable.as_poker)
+            2 -> resultado.setImageResource(R.drawable._5128583_poker_naipe_2_coraz_n)
+            3 -> resultado.setImageResource(R.drawable.depositphotos_83316660_stock_illustration_poker_playing_card_3_diamond)
+            4 -> resultado.setImageResource(R.drawable._4712945_gran__ndice_de_juego_de_cartas_4_de_espadas)
+            5 -> resultado.setImageResource(R.drawable._5128550_poker_naipe_5_coraz_n)
+            6 -> resultado.setImageResource(R.drawable.descarga)
+            7 -> resultado.setImageResource(R.drawable._5128571_cartas_de_p_quer_juego_7_del_club)
+            8 -> resultado.setImageResource(R.drawable.depositphotos_83317278_stock_illustration_poker_playing_card_8_diamond)
+            9 -> resultado.setImageResource(R.drawable._5128581_poker_tarjeta_de_juego_9_coraz_n)
+            10 -> resultado.setImageResource(R.drawable._5128553_poker_naipe_10_coraz_n)
+            11 -> resultado.setImageResource(R.drawable._4712993_gran__ndice_de_juego_de_cartas_de_diamantes)
+            12 -> resultado.setImageResource(R.drawable.dame_de_coeur)
+            13 -> resultado.setImageResource(R.drawable.af3050cb31bc90ad1952b782489fcfb5)
+            14 -> resultado.setImageResource(R.drawable._4712945_gran__ndice_de_juego_de_cartas_4_de_espadas+R.drawable._5128553_poker_naipe_10_coraz_n)
+            15 -> resultado.setImageResource(R.drawable._5128550_poker_naipe_5_coraz_n+R.drawable._5128553_poker_naipe_10_coraz_n)
+            16 -> resultado.setImageResource(R.drawable.descarga +R.drawable._5128553_poker_naipe_10_coraz_n)
+            17 -> resultado.setImageResource(R.drawable._5128571_cartas_de_p_quer_juego_7_del_club +R.drawable._5128553_poker_naipe_10_coraz_n)
+            18 -> resultado.setImageResource(R.drawable.depositphotos_83317278_stock_illustration_poker_playing_card_8_diamond +R.drawable._5128553_poker_naipe_10_coraz_n)
+        }
     }
 }
